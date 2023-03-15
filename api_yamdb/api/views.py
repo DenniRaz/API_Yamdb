580713530691c76d3d82d222bc10427d6201621e
Алексей Филин <alexey.filin92@gmail.com>
2023-03-15 22:27:16 +0300

    Исправленная ветка develop (ошибки с миграциями)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,19 @@
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-from reviews.models import Category, Genre, Title, Review
-from .serializers import (CategorySerializer, GenreSerializer,
-                          TitleSerializer, ReadOnlyTitleSerializer,
-                          CommentSerializer, ReviewSerializer)
-from .permissions import IsAdminOrReadOnly
-from mixins import ListCreateDestroyViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
-from rest_framework import pagination
+from rest_framework import filters, pagination, viewsets
+from reviews.models import Category, Genre, Review, Title
+
+from .mixins import ListCreateDestroyViewSet
+from .permissions import IsAdminOrReadOnly
+from .serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    ReadOnlyTitleSerializer,
+    ReviewSerializer,
+    TitleSerializer,
+)
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
