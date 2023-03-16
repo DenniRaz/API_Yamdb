0d9828721a951f6851caa4d6abbd8e7c6b3d2c1a
Сергей Жуков <Yorgankrait@gmail.com>
2023-03-16 18:01:14 +0300

    не получилось

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,38 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    ADMIN = 'admin'
-    MODERATOR = 'moderator'
-    USER = 'user'
-    ROLES = [
-        (ADMIN, 'Administrator'),
-        (MODERATOR, 'Moderator'),
-        (USER, 'User'),
-    ]
-
-    email = models.EmailField(
-        verbose_name='Адрес электронной почты',
-        unique=True,
-    )
-    username = models.CharField(
-        verbose_name='Имя пользователя',
-        max_length=150,
-        null=True,
-        unique=True
-    )
-    role = models.CharField(
-        verbose_name='Роль',
-        max_length=50,
-        choices=ROLES,
-        default=USER
-    )
-    bio = models.TextField(
-        verbose_name='О себе',
-        null=True,
-        blank=True
-    )
 
 
 class Category(models.Model):
