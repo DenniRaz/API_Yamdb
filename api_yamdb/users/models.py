5ebb8fb7c5c5e356617b58232a799288aa0fdccf
Сергей Жуков <Yorgankrait@gmail.com>
2023-03-20 20:46:39 +0300

    41 failed/36 passed

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,14 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-CHOICES = (
-    ('user', 'Аутентифицированный пользователь'),
-    ('moderator', 'Модератор'),
-    ('admin', 'Администратор'),
-)
-
 
 class User(AbstractUser):
+    is_user = 'user'
+    is_moderator = 'moderator'
+    is_admin = 'admin'
+    CHOICES = [
+        (is_user, 'Аутентифицированный пользователь'),
+        (is_moderator, 'Модератор'),
+        (is_admin, 'Администратор'),
+    ]
+
     bio = models.TextField(
         'Биография',
         blank=True,
@@ -18,6 +21,21 @@ class User(AbstractUser):
         default='user',
         max_length=16,
     )
+
+
+@property
+def is_admin(self):
+    return self.role == self.admin
+
+
+@property
+def is_moderator(self):
+    return self.role == self.moderator
+
+
+@property
+def is_authenticated(self):
+    return self.role == self.user
 
 
 class EmailVerification(models.Model):
