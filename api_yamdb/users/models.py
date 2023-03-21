b2270e33035bf6277549383aada7764f4ec20e43
Алексей Филин <alexey.filin92@gmail.com>
2023-03-21 14:29:59 +0300

    25 failed, 52 passed

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
-    is_user = 'user'
-    is_moderator = 'moderator'
-    is_admin = 'admin'
+    user = 'user'
+    moderator = 'moderator'
+    admin = 'admin'
     CHOICES = [
-        (is_user, 'Аутентифицированный пользователь'),
-        (is_moderator, 'Модератор'),
-        (is_admin, 'Администратор'),
+        (user, 'Аутентифицированный пользователь'),
+        (moderator, 'Модератор'),
+        (admin, 'Администратор'),
     ]
 
     bio = models.TextField(
@@ -22,20 +22,22 @@ class User(AbstractUser):
         max_length=16,
     )
 
+    def save(self, *args, **kwargs):
+        if self.is_superuser is True:
+            self.role = self.admin
+        super().save(*args, **kwargs)
 
-@property
-def is_admin(self):
-    return self.role == self.admin
+    @property
+    def is_admin(self):
+        return self.role == self.admin
 
+    @property
+    def is_moderator(self):
+        return self.role == self.moderator
 
-@property
-def is_moderator(self):
-    return self.role == self.moderator
-
-
-@property
-def is_authenticated(self):
-    return self.role == self.user
+    @property
+    def is_user(self):
+        return self.role == self.user
 
 
 class EmailVerification(models.Model):
